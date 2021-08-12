81a379c62cde2d04db6270711e4863feea3a4a1b
Bill <billaoun@gmail.com>
2021-08-12 11:02:32 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,77 +1,76 @@
-const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
 //play pause video
 function toggleVideoStatus() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-};
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 //update play pause icon
 function updatePlayIcon() {
-    if (video.paused) {
-        play.innerHTML = '<i class="fa fa-play fa-2x"><i/>'
-
-    } else {
-        play.innerHTML = '<i class="fa fa-pause fa-2x"><i/>'
-    }
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"><i/>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"><i/>';
+  }
 }
 
 //update progress and timestamp
 function updateProgress() {
-    progress.value = (video.currentTime / video.duration) * 100;
+  progress.value = (video.currentTime / video.duration) * 100;
 
-    //get mins
-    let mins = Math.floor(video.currentTime / 60);
+  //get mins
+  let mins = Math.floor(video.currentTime / 60);
 
-    if (mins < 10) {
-        mins = '0' + String(mins);
-    }
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
 
-    //get secs
-    let secs = Math.floor(video.currentTime % 60);
-    if (secs < 10) {
-        secs = '0' + String(secs);
-
-    }
-    timestamp.innerHTML = `${mins}:${secs}`;
-
-
-
-
+  //get secs
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 //set video time to progress
 function setVideoProgress() {
-    video.currentTime = (+progress.value * video.duration) / 100;
+  video.currentTime = (+progress.value * video.duration) / 100;
+  console.log(progress.value);
 }
 
 // stop video
 function stopVideo() {
-    video.currentTime = 0;
-    video.pause();
+  video.currentTime = 0;
+  video.pause();
 }
 
+//toggle fullscreen
 
-
-
-
-
+function toggleFullScreen() {
+  const rfs =
+    video.requestFullscreen ||
+    video.webkitRequestFullScreen ||
+    video.mozRequestFullScreen ||
+    video.msRequestFullscreen;
+  rfs.call(video);
+}
 
 //event listeners
-video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeupdate', updateProgress);
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("dblclick", toggleFullScreen);
 
-
-play.addEventListener('click', toggleVideoStatus);
-stop.addEventListener('click', stopVideo);
-progress.addEventListener('change', setVideoProgress);
-
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
